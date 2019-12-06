83236d53b538a540dc34342d094f4593e724aab8
Julianevinska <57527101+Julianevinska@users.noreply.github.com>
2019-12-06 11:40:08 +0200

    HW 4.3 - solution

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String isPalindrome = text.toLowerCase().replaceAll("[^a-z0-9]", "");
+        StringBuilder stringBuilder = new StringBuilder(isPalindrome);
+        return isPalindrome.equals(stringBuilder.reverse().toString());
     }
 }
